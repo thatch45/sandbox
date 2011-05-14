baf181b7a3199abbc70088238f3c468a518e1ed6
Thomas S Hatch <thatch45@gmail.com>
2011-05-13 21:04:39 -0600

    Add dome comments to the simple file server

--- examples/zmq/file_get.py
+++ examples/zmq/file_get.py
@@ -6,20 +6,25 @@ import sys
 
 def get_file(path):
     '''
+    A VERY simple client to get files from the zeromq VERY simple file server
     '''
+    # Open up the file we are going to write to
     dest = open(os.path.basename(path), 'w+')
+    # Set up the zeromq context and socket
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
 
     socket.connect('tcp://127.0.0.1:4545')
-    socket.setsocketopt(zmq.REQUEST, '')
+    # send the desired file to the server
+    socket.send(path)
 
     while True:
+        # Start grabing data
         data = socket.recv()
-        if not data:
-            break
+        # Write the chunk to the file
         dest.write(data)
-        if not socket.getsocketopt():
+        if not socket.getsockopt(zmq.RCVMORE):
+            # If there is not more data to send, then break
             break
 
 if __name__ == '__main__':
--- examples/zmq/file_serve.py
+++ examples/zmq/file_serve.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python2
 
 import zmq
+import os
 
 def server():
     '''
     Setup a VERY simple file server
     '''
+    # Set up the zeromq context and REP socket
     context = zmq.Context(1)
     sock = context.socket(zmq.REP)
     sock.bind('tcp://*:4545')
 
+    # Start the server loop
     while True:
+        # Recieve the location of the file to serve
         msg = sock.recv()
+        # Verify that the file is available
         if not os.path.isfile(msg):
             sock.send('')
             continue
+        # Open the file for reading
         fn = open(msg, 'rb')
         stream = True
+        # Start reading in the file
         while stream:
+            # Read the file bit by bit
             stream = fn.read(128)
             if stream:
+                # If the stream has more to send then send more
                 sock.send(stream, zmq.SNDMORE)
             else:
+                # Finish it off
                 sock.send(stream)
 
 if __name__ == '__main__':
