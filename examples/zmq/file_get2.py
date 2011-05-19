b4102f5d42ca9fbd2d148d0617d8785e6a8a61d1
Thomas S Hatch <thatch45@gmail.com>
2011-05-19 16:09:41 -0600

    Another try at the file server

---
+++ examples/zmq/file_get2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python2
+
+import zmq 
+import os
+import sys
+
+def get_file(path):
+    '''
+    A VERY simple client to get files from the zeromq VERY simple file server
+    '''
+    # Open up the file we are going to write to
+    dest = open(os.path.basename(path), 'w+')
+    # Set up the zeromq context and socket
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+
+    socket.connect('tcp://127.0.0.1:4545')
+
+    msg = {'loc': 0,
+           'path': path}
+
+    while True:
+        # send the desired file and the location to the server
+        socket.send_pyobj(path)
+        # Start grabing data
+        data = socket.recv()
+        # Write the chunk to the file
+        if data['body']:
+            dest.write(data['body'])
+            msg['loc'] = dest.tell()
+        else:
+            break
+
+if __name__ == '__main__':
+    get_file(sys.argv[1])
---
+++ examples/zmq/file_serve2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python2
+
+import zmq
+import os
+
+def server():
+    '''
+    Setup a VERY simple file server
+    '''
+    # Set up the zeromq context and REP socket
+    context = zmq.Context(1)
+    sock = context.socket(zmq.REP)
+    sock.bind('tcp://*:4545')
+
+    BUFF = 64
+
+    # Start the server loop
+    while True:
+        # Set up a return container
+        ret = {}
+        # Recieve the location of the file to serve
+        msg = sock.recv_pyobj()
+        # Verify that the file is available
+        if not os.path.isfile(msg['path']):
+            sock.send('')
+            continue
+        # Open the file for reading
+        fn = open(msg, 'rb')
+        fn.seek(msg['loc'])
+        ret['body'] = fn.read(BUFF)
+        ret['loc'] = fn.tell()
+        sock.send_pyobj(ret)
+
+if __name__ == '__main__':
+    server()
