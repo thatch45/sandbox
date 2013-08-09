631ebfccb17bcc1fd1509ded910d38de7a434ddf
Thomas S Hatch <thatch45@gmail.com>
2013-08-09 11:26:41 -0600

    Add compare test

---
+++ performance/comp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+
+import time
+
+stuff = range(0, 10000000)
+key = 'arm'
+start = time.time()
+for i in stuff:
+    key != 'amd64' and key != 'all'
+print(time.time() - start)
+
+start = time.time()
+for i in stuff:
+    key not in ('amd64', 'all')
+print(time.time() - start)
